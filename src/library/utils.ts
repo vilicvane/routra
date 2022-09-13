74c270ff145a7b1983779ad2d66bcca664457ee1
vilicvane <vilicvane@live.com>
2022-09-13 19:05:40 +0800

    Add utility types

--- src/library/utils.ts
+++ src/library/utils.ts
@@ -1,3 +1,14 @@
 import type {__Route} from './route';
+import type {_RouterClass, __Router} from './router';
+import type {_Transition} from './transition';
 
 export type RouteView<TRoute extends __Route> = TRoute['$views'][number];
+
+export type Transition<TRouter extends __Router> = _Transition<
+  TransitionState<TRouter>
+>;
+
+export type TransitionState<TRouter extends __Router> =
+  TRouter extends _RouterClass<unknown, unknown, infer TTransitionState>
+    ? TTransitionState
+    : never;
